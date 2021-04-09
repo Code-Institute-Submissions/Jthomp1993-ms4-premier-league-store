6a349e85a4e154901370b26c25e89d5f9d09b391
Jthomp1993 <joshuagt93@hotmail.com>
2021-04-09 13:49:30 +0000

    Added additional functionality to sort by price, rating and category

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category
+from .models import Product, Category, Team
 
 # Create your views here.
 
@@ -12,8 +12,28 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    teams = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'team' in request.GET:
+            teams = request.GET['team'].split(',')
+            products = products.filter(team__name__in=teams)
+            teams = Team.objects.filter(name__in=teams)
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -30,10 +50,14 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_team': teams,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
