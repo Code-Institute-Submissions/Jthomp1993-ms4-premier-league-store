60a21a21a6aa7879d5788e65eb8e6cb61e963c75
Jthomp1993 <joshuagt93@hotmail.com>
2021-04-15 15:14:16 +0000

    Added toast files and success message to add to cart view

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -11,6 +13,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add quantity of selected product to shopping cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -31,6 +34,7 @@ def add_to_cart(request, item_id):
             cart[item_id] += quantity
         else:
             cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag.')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
