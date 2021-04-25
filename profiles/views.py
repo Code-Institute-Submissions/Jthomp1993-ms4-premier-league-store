2fede106c85d8177720bb14fa67905e7a9cbef76
Jthomp1993 <joshuagt93@hotmail.com>
2021-04-25 16:24:03 +0000

    Finished the add product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -10,7 +11,16 @@ def profile(request):
     """ Display the users profile """
     profile = get_object_or_404(UserProfile, user=request.user)
 
-    form = UserProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
     orders = profile.orders.all()
     template = 'profiles/profile.html'
     context = {
