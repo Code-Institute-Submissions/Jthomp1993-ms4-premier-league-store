68ba4acda4e358d0e22194ff2d780107075cfd0f
Jthomp1993 <joshuagt93@hotmail.com>
2021-04-25 11:36:04 +0000

    Created the news app, views and template

--- news/models.py
+++ news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=40)
+    heading = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(max_length=254, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
