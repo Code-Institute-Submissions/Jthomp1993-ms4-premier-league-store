72d1e35eb4f9d744668a194843ef475bdeb1f5e2
Jthomp1993 <joshuagt93@hotmail.com>
2021-04-26 19:49:20 +0000

    Added comments model and updated news_detail.html

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import News
+from .models import News, Comments
 
 
 class NewsAdmin(admin.ModelAdmin):
@@ -12,5 +12,6 @@ class NewsAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(News, NewsAdmin)
+admin.site.register(News, NewsAdmin,)
+admin.site.register(Comments)
 
--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from profiles.models import UserProfile
 
 
 class News(models.Model):
@@ -14,3 +15,17 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comments(models.Model):
+    class Meta:
+        verbose_name_plural = 'Comments'
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL,
+        null=True, blank=True, default='Anonymous')
+    comment = models.TextField()
+    date_time = models.DateTimeField(auto_now_add=True,)
+
+    def __str__(self):
+        return self.comment
