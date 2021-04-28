deecf7defcc0cbade50a63077b7a5007ddddaa1d
Jthomp1993 <joshuagt93@hotmail.com>
2021-04-28 13:55:19 +0000

    Created heroku app and linked to postgres database

--- news/models.py
+++ news/models.py
@@ -7,7 +7,7 @@ class News(models.Model):
         verbose_name_plural = 'News'
 
     title = models.CharField(max_length=120)
-    heading = models.CharField(max_length=100, null=False, blank=False)
+    heading = models.CharField(max_length=150, null=False, blank=False)
     description = models.TextField(max_length=254, null=False, blank=False)
     content = models.TextField(null=False, blank=False)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
--- premier_league_store/settings.py
+++ premier_league_store/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,9 +126,9 @@ WSGI_APPLICATION = 'premier_league_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
 }
 
 
